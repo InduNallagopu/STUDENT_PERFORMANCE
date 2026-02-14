5a3e11327ac35658574a0459d78d3e4183584515
InduNallagopu <indunallagopu@gmail.com>
2026-02-14 16:24:54 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+hypen_e_dot = '-e .'
+def get_requirements(file_path):
+    '''this function will return the list of requirements'''
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements
+
+setup(
+    name='student_performance',
+    version='0.1.0',
+    author='InduNallagopu',
+    author_email='indunallagopu@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
